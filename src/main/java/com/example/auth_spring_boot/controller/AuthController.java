c64b01605be2e2d89cb3388d484d133c2a651063
ghnimihassen <ghnimih@gmail.com>
2025-05-05 20:52:51 +0100

    add model post

--- src/main/java/com/example/auth_spring_boot/controller/AuthController.java
+++ src/main/java/com/example/auth_spring_boot/controller/AuthController.java
@@ -22,7 +22,7 @@ public class AuthController {
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
-
+//login
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
